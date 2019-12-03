ec438aa20a0aac13d443af742135036608e967a1
Wiedemann <ws21@pop-os.localdomain>
2019-12-04 00:45:20 +0100

    day 3 challenge 2

--- 3/main.py
+++ 3/main.py
@@ -19,9 +19,16 @@ def getCross(pT1, t1, pT2, t2):
 	else: #y is equal 
 		return (t2[0], t1[1])
 
+def getStepsToCross(pT1, t1, pT2, t2):
+	if pT1[0] == t1[0]: #x is equal
+		return abs(pT1[1] - t2[1]) + abs(pT2[0] - t1[0])
+	else: #y is equal 
+		return abs(pT1[0] - t2[0]) + abs(pT2[1] -t1[1])
+
 def getTurns(turns, wire):
 	posX = 0
 	posY = 0
+	idx = 0
 	for p in wire:
 		d = p[0]
 		l = int(p[1:])
@@ -37,7 +44,9 @@ def getTurns(turns, wire):
 			yDir = 1
 		posX = posX + xDir*l
 		posY = posY + yDir*l
-		turns.append((posX,posY, d))
+		steps = abs(xDir*l + yDir*l)
+		turns.append((posX,posY, d, steps))
+		idx += 1
 
 def main():
 	lines = sys.stdin
@@ -57,23 +66,35 @@ def main():
 	getTurns(turns2, wire2)		
 
 	minDist = 99999
+	minDistSteps = 99999
 	x = None
+	minSteps = 99999
 	pT1 = turns1[0]
 	pT2 = turns2[0]
+	stepsT1 = 0
 	for t1 in turns1:
+		stepsT1 += t1[3]
+		steps = stepsT1
 		for t2 in turns2:
 			if crossed(pT1, t1, pT2, t2):
 				xtemp = getCross(pT1, t1, pT2, t2)
+				stc = getStepsToCross(pT1, t1, pT2, t2)
+				tempsteps = steps - t1[3] + stc
 				dtemp = abs(xtemp[0]) + abs(xtemp[1])
 				if dtemp < minDist:
 					x = xtemp
 					minDist = dtemp
+					minDistSteps = tempsteps
+				if tempsteps < minSteps:
+					minSteps = tempsteps
+
+			steps += t2[3]
 					
 			
 			pT2 = t2
 		pT1 = t1 
 	
-	print(x, minDist)
+	print(x, minDist,minDistSteps, minSteps)
 
 if __name__== "__main__":
 	main()

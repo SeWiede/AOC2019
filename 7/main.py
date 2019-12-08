a37ccb23eff3ea65da7e263defc705829d32e107
Wiedemann <ws21@pop-os.localdomain>
2019-12-08 04:05:18 +0100

    day 7 1

--- 7/main.py
+++ 7/main.py
@@ -21,20 +21,53 @@ def getAMP(setting, AMP):
 
 	return ret
 
+def startAmp(setting):
+	prog = subprocess.Popen(["python", "../5/main.py", sys.argv[1]],
+							stdin=subprocess.PIPE,
+							stdout=subprocess.PIPE)
+	prog.stdin.write(str(setting)+"\n")
+	
+	return prog
+
+def getAMPProg(prog, AMP):
+	prog.stdin.write(str(AMP) + "\n")
+	ret = -1
+	x = ""
+	try:
+		x = prog.stdout.readline()
+		ret = int(x)
+	except:
+		print(x)
+
+	return ret
 
 def main():
 	
 	perms = list(itertools.permutations([0,1,2,3,4]))
+	perms2 = list(itertools.permutations([5,6,7,8,9]))
 	test = [0,1,2,3,4]	
 
 	maxthrust = 0
 	maxperm= []
-	for cl in perms:
-		AAmp = getAMP(cl[0], 0)
-		BAmp = getAMP(cl[1], AAmp)
-		CAmp = getAMP(cl[2], BAmp)
-		DAmp = getAMP(cl[3], CAmp)
-		EAmp = getAMP(cl[4], DAmp)
+	for cl in perms2:
+		A = startAmp(cl[0])
+		B = startAmp(cl[1])
+		C = startAmp(cl[2])
+		D = startAmp(cl[3])
+		E = startAmp(cl[4])
+		AAmp = getAMPProg(A, 0)
+		BAmp = getAMPProg(B, AAmp)
+		CAmp = getAMPProg(C, BAmp)
+		DAmp = getAMPProg(D, CAmp)
+		EAmp = getAMPProg(E, DAmp)
+		while True:
+			AAmp = getAMPProg(A, EAmp)
+			BAmp = getAMPProg(B, AAmp)
+			CAmp = getAMPProg(C, BAmp)
+			DAmp = getAMPProg(D, CAmp)
+			EAmp = getAMPProg(E, DAmp)
+			if E.poll() != None:
+				break	
 		if EAmp > maxthrust:
 			maxthrust = EAmp
 			maxperm = cl
